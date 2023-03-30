c34c01ed24ca509d2b8ad6f6eea2185aa023c974
linupy <linupy@163.com>
2023-03-31 00:55:45 +0800

    第6章 使用模版上下文处理函数

--- chapter06/app.py
+++ chapter06/app.py
@@ -1,6 +1,11 @@
 from flask import Flask, url_for, render_template
+from flask_pymongo import PyMongo
 
 app = Flask(__name__)
+
+db_flask_demo = PyMongo(
+    app,
+    uri='mongodb://admin:12345678@localhost:27017/flask_demo?authSource=admin')
 
 
 @app.errorhandler(404)
@@ -10,9 +15,22 @@ def page_not_found(e):
     返回值作为相应主体返回客户端
     """
     # 后跟status_code
-    return render_template('404.html', user='zhangsan'), 404
+    return render_template('404.html'), 404
+
+
+@app.context_processor
+def inject_user():
+    """
+    使用装饰器注册一个模版上下文处理函数
+    """
+    user = db_flask_demo.db.user.find_one({}, {'_id': 0})
+    # 函数返回的变量（以字典键值对的形式），
+    # 将会统一注入到每一个模板的上下文环境中，因此可以直接在模板中使用
+    return {'user': user}
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    movies = db_flask_demo.db.movie.find({}, {'_id': 0})
+    movies = [movie for movie in movies]
+    return render_template('index.html', movies=movies)
